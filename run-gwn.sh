d959b5a247c5c2dcf8f8011f3444b6d23d477650
Lea <autoit4you@users.noreply.github.com>
2025-12-20 17:33:27 +0100

    fix run script

--- run-gwn.sh
+++ run-gwn.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ ! -f /gwn/data/.GWN_MANAGER_DOCKER ] || [ "$GWN_MANAGER_VERSION" != "$(cat /gwn/data/.GWN_MANAGER_DOCKER)"]; then
+if [ ! -f /gwn/data/.GWN_MANAGER_DOCKER ] || [ "$GWN_MANAGER_VERSION" != "$(cat /gwn/data/.GWN_MANAGER_DOCKER)" ]; then
     dpkg -i /GWN_Manager/packages/*.deb
     /gwn/scripts/install_gwn.sh "/GWN_Manager/packages/GWN_Manager_Upgrade_$GWN_MANAGER_VERSION.tar.gz" notstart
     echo "$GWN_MANAGER_VERSION" > /gwn/data/.GWN_MANAGER_DOCKER
@@ -9,4 +9,4 @@ fi
 chown gwn:gwn /gwn/data /gwn/logs /gwn_backup
 
 /gwn/gwn start
-tail -f /gwn/logs/**/*.log /gwn/logs/**/**/*.log /gwn/logs/**/**/*.out
+tail -F /gwn/logs/* 2>/dev/null
